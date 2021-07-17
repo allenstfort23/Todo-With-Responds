8d7ccf53aeaf946ae632852232aff85fdcbec060
Allen St Fort <allenstfort1@gmail.com>
2021-07-17 18:12:13 +0000

    React with fetch set up

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,85 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	// const [isShown, setIsShown] = useState(false);
+	const [variable, setVariable] = useState(null);
+
+	const apiUrl =
+		"https://assets.breatheco.de/apis/fake/todos/user/allenstfort";
+
+	useEffect(() => {
+		if (variable !== null) {
+			fetch(apiUrl, {
+				method: "PUT",
+				body: JSON.stringify(variable),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+		} else {
+			fetch(apiUrl)
+				.then(res => res.json())
+				.then(newTodo => setVariable(newTodo))
+				.catch(error => console.log(error));
+		}
+	}, [variable]);
+
+	let todo = (variable || []).map((item, i) => {
+		return (
+			<li className="list-group-item" key={i}>
+				{item.label}
+				<button onClick={() => removeItem(i)}>X</button>
+			</li>
+		);
+	});
+
+	const removeItem = index => {
+		console.log(index);
+		const newArray = variable.filter((item, i) => i != index);
+		// const newArray = variable.filter((item, i) => {
+		// 	if (i != index) {
+		// 		return item;
+		// 	}
+		// });
+		setVariable(newArray);
+	};
+
+	const newTodo = onKeyDownEvent => {
+		console.log(onKeyDownEvent);
+		if (onKeyDownEvent.keyCode === 13) {
+			let userInput = {
+				label: onKeyDownEvent.target.value,
+				done: false
+			};
+			const newTodo = [...variable, userInput];
+			setVariable(newTodo);
+			onKeyDownEvent.target.value = "";
+			// fetch(apiUrl, {
+			// 	method: "PUT",
+			// 	body: JSON.stringify(newTodo),
+			// 	headers: {
+			// 		"Content-Type": "application/json"
+			// 	}
+			// });
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="List ">
+			<h1 className="title">To Do List</h1>
+			<input
+				className={"input"}
+				onKeyDown={newTodo}
+				type="text"
+				id="fname"
+				placeholder="Task"
+				name="fname"></input>
+			<div>
+				<ul className="list-group">{todo}</ul>
+				<div className="footer">{todo.length} item left</div>
+			</div>
 		</div>
 	);
 }
+export default Home;
